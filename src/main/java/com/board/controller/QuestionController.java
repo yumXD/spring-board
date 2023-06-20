1085aa9cd1e6522842039e6f5f721750de7be07a
yumXD <qkpw99@gmail.com>
2023-06-20 17:19:00 +0900

    feat: 질문 목록 페이지 처리

---
+++ src/main/java/com/board/controller/QuestionController.java
@@ -0,0 +1,25 @@
+package com.board.controller;
+
+import com.board.entity.Question;
+import com.board.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/question")
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+    private final QuestionService questionService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
---
+++ src/main/java/com/board/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.board.service;
+
+import com.board.entity.Question;
+import com.board.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+}
