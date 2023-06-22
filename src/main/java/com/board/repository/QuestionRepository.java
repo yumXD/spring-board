e550b8cb970f2597680d30784b07440056fbd8ee
yumXD <qkpw99@gmail.com>
2023-06-22 14:43:00 +0900

    feat: 질문 목록 페이징 처리

--- src/main/java/com/board/repository/QuestionRepository.java
+++ src/main/java/com/board/repository/QuestionRepository.java
@@ -1,9 +1,12 @@
 package com.board.repository;
 
 import com.board.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Page<Question> findAll(Pageable pageable);
 }
--- src/main/java/com/board/service/QuestionService.java
+++ src/main/java/com/board/service/QuestionService.java
@@ -4,9 +4,14 @@ import com.board.entity.Question;
 import com.board.repository.QuestionRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,8 +20,11 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 5, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public void create(String subject, String content) {
