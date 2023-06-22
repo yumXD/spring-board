ada5526c25ce9eed6256cd87b43ad4ed2b615097
yumXD <qkpw99@gmail.com>
2023-06-22 10:41:00 +0900

    feat: 답변 등록 페이지 처리

--- src/main/java/com/board/controller/QuestionController.java
+++ src/main/java/com/board/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.board.controller;
 
+import com.board.dto.AnswerForm;
 import com.board.dto.QuestionForm;
 import com.board.entity.Question;
 import com.board.service.QuestionService;
@@ -47,6 +48,7 @@ public class QuestionController {
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
+        model.addAttribute("answerForm", new AnswerForm());
         return "question_detail";
     }
 }
---
+++ src/main/java/com/board/service/AnswerService.java
@@ -0,0 +1,25 @@
+package com.board.service;
+
+import com.board.entity.Answer;
+import com.board.entity.Question;
+import com.board.repository.AnswerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
