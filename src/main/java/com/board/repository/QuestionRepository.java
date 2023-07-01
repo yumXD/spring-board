a9adcc61d3066b773039ee7369797bf25ec95f3b
yumXD <qkpw99@gmail.com>
2023-07-01 15:11:00 +0900

    feat: 질문 검색 기능

--- src/main/java/com/board/repository/QuestionRepository.java
+++ src/main/java/com/board/repository/QuestionRepository.java
@@ -3,10 +3,29 @@ package com.board.repository;
 import com.board.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Page<Question> findAll(Pageable pageable);
+
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+
+    @Query("select "
+            + "distinct q "
+            + "from Question q "
+            + "left outer join SiteUser u1 on q.author=u1 "
+            + "left outer join Answer a on a.question=q "
+            + "left outer join SiteUser u2 on a.author=u2 "
+            + "where "
+            + "   q.subject like %:kw% "
+            + "   or q.content like %:kw% "
+            + "   or u1.username like %:kw% "
+            + "   or a.content like %:kw% "
+            + "   or u2.username like %:kw% ")
+    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
